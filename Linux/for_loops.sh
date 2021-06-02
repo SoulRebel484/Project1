8750c483af8f7faaa8912553ff2e957db7f32333
soulrebel484 <soulrebel484@hotmail.com>
2021-06-02 15:28:06 -0400

    first commit

---
+++ Linux/for_loops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+states=('pennsylvania' 'california' 'north carolina' 'new jersey' texas')
+for state in ${states[@]}
+do
+   if [ $state == 'hawaii' ]
+    then
+        echo "Hawaii is the best!"
+     else
+        echo "Im not fond of Hawaii"
+    fi
+done
---
+++ Linux/sys.info.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "todays date and system info is"
+date
+uname
+hostname
